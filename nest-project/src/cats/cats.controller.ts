a1b1af87aa59cc57f02210355e2520914acde8c9
JSN_dev <dev2.jsninjas@gmail.com>
2019-05-08 11:01:24 +0300

    Added CRUD for cats module

--- nest-project/src/cats/cats.controller.ts
+++ nest-project/src/cats/cats.controller.ts
@@ -1,20 +1,32 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
-  constructor(private readonly catsService: CatsService) {}
-
-  @Post()
-  async create(@Body() createCatDto: CreateCatDto) {
-    this.catsService.create(createCatDto);
-  }
+  constructor(private readonly catsService: CatsService) {} 
 
   @Get()
-  async findAll(): Promise<Cat[]> {
+  findAll(): Promise<Cat[]> {
     return this.catsService.findAll();
   }
+
+  @Get(':id')
+  findOne(@Param('id') id): Promise<Cat> {
+    return this.catsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto): Promise<Cat> {
+    return this.catsService.create(createCatDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): Promise<Cat> {
+    console.log('ID', id)
+    return this.catsService.delete(id);
+  }
+
 }
 
--- nest-project/src/cats/cats.service.ts
+++ nest-project/src/cats/cats.service.ts
@@ -2,33 +2,27 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Cat } from './interfaces/cat.interface';
-import { CreateCatDto } from './dto/create-cat.dto';
+//import { CreateCatDto } from './dto/create-cat.dto';
 
 @Injectable()
 export class CatsService {
   constructor(@InjectModel('Cat') private readonly catModel: Model<Cat>) {}
 
-  async create(createCatDto: CreateCatDto): Promise<Cat> {
-    const createdCat = new this.catModel(createCatDto);
-    return await createdCat.save();
+  async create(cat: Cat): Promise<Cat> {
+    const newCat = new this.catModel(cat);
+    return await newCat.save();
   }
 
   async findAll(): Promise<Cat[]> {
     return await this.catModel.find().exec();
   }
+
+  async findOne(id: string): Promise<Cat> {
+    return await this.catModel.findOne({ _id: id })
+  }
+
+  async delete(id: string): Promise<Cat> {
+    return await this.catModel.findByIdAndRemove(id);
+  }
+
 }
-// import { Injectable } from '@nestjs/common';
-// import { Cat } from './interfaces/cat.interface';
-
-// @Injectable()
-// export class CatsService {
-//   private readonly cats: Cat[] = [];
-
-//   create(cat: Cat) {
-//     this.cats.push(cat);
-//   }
-
-//   findAll(): Cat[] {
-//     return this.cats;
-//   }
-// }
